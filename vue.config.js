ef3d8c65797d9ae037e3b090d1b21b59a120d79c
wu-medoc <58807721+wu-medoc@users.noreply.github.com>
2024-02-16 21:26:58 +0800

    node_v14.21.3 npm_9.6.1

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production',
   publicPath: '/vue3/',
+
   chainWebpack: config => {
     config.performance
       .maxEntrypointSize(1000000)
       .maxAssetSize(1000000)
-  }
+  },
+
+  productionSourceMap: false
 }
